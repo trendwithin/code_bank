241b40e9e92e89696f5b8da5417a943230ad1da7
Michael Becco <e4e6d4d5@gmail.com>
2015-03-04 14:38:21 -0800

    Modify Test Case

--- deck_of_cards/spec/features/card_deck_spec.rb
+++ deck_of_cards/spec/features/card_deck_spec.rb
@@ -1,6 +1,6 @@
 require 'minitest/spec'
 require 'minitest/autorun'
-require 'cards'
+require 'deck_of_cards'
 
 describe DeckOfCards do
   it "has an Ace of Hearts" do
@@ -16,5 +16,15 @@ describe DeckOfCards do
   it "has the Deuce of Hearts" do
     c = DeckOfCards.new
     c.full_deck.must_include "2H"
+  end
+
+  it "has the 3 of Clubs" do
+    c = DeckOfCards.new
+    c.full_deck.must_include "3C"
+  end
+
+  it "has 52 cards" do
+    c = DeckOfCards.new
+    c.full_deck.length.must_equal 52
   end
 end
